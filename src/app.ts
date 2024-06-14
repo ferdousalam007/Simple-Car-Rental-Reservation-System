d1caf98de87e70f8238f3f18ca8e0f5555c7fe88
ferdousalam007 <iamferdousalam007@gmail.com>
2024-06-14 12:16:06 +0600

    file setup for requirement for this project

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// application routes
+app.use('/api/v1', router);
+
+
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
+
+export default app;
---
+++ src/app/routes/index.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
+  },
+ 
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
